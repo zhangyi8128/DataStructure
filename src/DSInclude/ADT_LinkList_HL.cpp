9dae4ffa7ff7f659673ca9e04133bdc28cec2e9a
test <test@126.com>
2021-07-17 18:42:16 +0800

    带头结点的链表的部分的实现与测试

--- src/DSInclude/ADT_LinkList_HL.cpp
+++ src/DSInclude/ADT_LinkList_HL.cpp
@@ -5,9 +5,10 @@
 #include "ADT_LinkList_HL.h"
 #include <iostream>
 
+
 void ADT_LinkList_HL::test() {
     LinkList L;
-    Link p, q;
+    Link p, q, r;
     Status status;
     ElemType e;
     // 内存对齐
@@ -46,6 +47,8 @@ void ADT_LinkList_HL::test() {
     Append(L, q);
     std::cout << "q.data：" << q->data << std::endl;
 
+    std::cout << "======测试 Remove 方式=====" << std::endl;
+    Remove(L, r);
 
     std::cout << "======测试 DestroyList 方式=====" << std::endl;
     status = DestroyList(L);
@@ -67,11 +70,9 @@ void ADT_LinkList_HL::FreeNode(Link &p) {
 }
 
 Status ADT_LinkList_HL::InitList(LinkList &L) {
-    L.head = (Link) malloc(sizeof(LNode));
-    L.tail = (Link) malloc(sizeof(LNode));
-    if (L.head == nullptr || L.tail == nullptr) return ERROR;
+    L.head = L.tail = (Link) malloc(sizeof(LNode));
+    if (L.head == nullptr) return ERROR;
     L.head->next = nullptr;
-    L.tail->next = nullptr;
     L.len = 0;
     return OK;
 }
@@ -103,12 +104,14 @@ Status ADT_LinkList_HL::DestroyList(LinkList &L) {
     return OK;
 }
 
+// 长度没有修改！！！
 Status ADT_LinkList_HL::InsFirst(Link h, Link s) {
     s->next = h->next;
     h->next = s;
     return OK;
 }
 
+// 长度没有修改！！！
 Status ADT_LinkList_HL::DelFirst(Link h, Link &q) {
     Link p = h->next;
     q = p;
@@ -127,6 +130,7 @@ Status ADT_LinkList_HL::Append(LinkList &L, Link s) {
     } else {
         // 为空表时，头结点指向 s，p 指向 s
         L.head->next = s;
+        // s 可能为一串结点，length 不一定等于 1
         p = s;
         i = 1;
     }
@@ -137,6 +141,108 @@ Status ADT_LinkList_HL::Append(LinkList &L, Link s) {
     }
     L.tail->next = p;
     L.len += i;
+    return OK;
+}
+
+Status ADT_LinkList_HL::Remove(LinkList &L, Link &q) {
+    // 为空表时无法删除
+    if (L.len == 0) return ERROR;
+    // p 指向尾结点的前驱
+    LNode *p = L.head->next;
+    while (p->next != nullptr && p->next != L.tail->next) {
+        p = p->next;
+    }
+    // 尾结点返回给 q
+    q = L.tail->next;
+    // 从链表摘除尾结点
+    p->next = nullptr;
+    // 更改尾结点指向与链表长度
+    L.tail->next = p;
+    --L.len;
+    return OK;
+}
+
+Status ADT_LinkList_HL::InsBefore(LinkList &L, Link &p, Link s) {
+    if (L.len == 0) {
+        L.head = L.tail = s;
+        p = s;
+        ++L.len;
+        return OK;
+    }
+    if (s == nullptr) return OK;
+    if (p == nullptr) return ERROR;
+
+    Link q = L.head->next;
+    while (q->next != nullptr && q->next != p) q = q->next;
+    if (q->next == nullptr) return ERROR;
+    s->next = p;
+    q->next = s;
+    ++L.len;
+    p = s;
+
+    return 0;
+}
+
+Status ADT_LinkList_HL::InsAfter(LinkList &L, Link &p, Link s) {
+    if (L.len == 0) {
+        L.head = L.tail = s;
+        p = s;
+        ++L.len;
+        return OK;
+    }
+    if (s == nullptr) return OK;
+    if (p == nullptr) return ERROR;
+    s->next = p->next;
+    p->next = s;
+    ++L.len;
+    p = s;
+    return OK;
+}
+
+Status ADT_LinkList_HL::SetCurElem(Link &p, ElemType e) {
+    p->data = e;
+    return OK;
+}
+
+ElemType ADT_LinkList_HL::GetCurElem(Link p) {
+    return p->data;
+}
+
+Status ADT_LinkList_HL::ListEmpty(LinkList L) {
+    return L.len == 0 ? TRUE : FALSE;
+}
+
+int ADT_LinkList_HL::ListLength(LinkList L) {
+    return L.len;
+}
+
+Position ADT_LinkList_HL::GetHead(LinkList L) {
+    return L.head->next;
+}
+
+Position ADT_LinkList_HL::GetLast(LinkList L) {
+    return L.tail->next;
+}
+
+Position ADT_LinkList_HL::PriorPos(LinkList L, Link p) {
+    Link q = L.head->next;
+    while(q->next != nullptr && q->next != p) q = q->next;
+    return L.head->next == p ? nullptr : q;
+}
+
+Position ADT_LinkList_HL::NextPos(LinkList L, Link p) {
+    return L.tail->next == p ? nullptr : p->next;
+}
+
+Status ADT_LinkList_HL::LocatePos(LinkList L, int i, Link &p) {
+    if(i < 1 || i > L.len) return ERROR;
+    Link q = L.head->next;
+    int k = 1;
+    while (q != nullptr && k < i) {
+        q = q->next;
+        ++k;
+    }
+    p = q;
     return OK;
 }
 
